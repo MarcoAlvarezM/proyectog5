039453f868257706bcb0a346b3c56b53cc8eaaf7
MarcoAlvarezM <malvarez90617@ufide.ac.cr>
2026-03-09 14:50:26 -0600

    Primer commit creacion

---
+++ src/main/java/com/proyectog5/Proyectog5Application.java
@@ -0,0 +1,13 @@
+package com.proyectog5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectog5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectog5Application.class, args);
+	}
+
+}
